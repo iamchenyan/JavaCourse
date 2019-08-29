cab0911e05ae8ed5b0f99934ab0eabc3c26ef3c3
chenyan <chenyandream@outlook.com>
2019-08-29 16:13:49 +0800

    a

---
+++ spring-shiro/src/main/java/com/chenyan/springshiro/web/shiro/filter/SysUserFilter.java
@@ -0,0 +1,31 @@
+package com.chenyan.springshiro.web.shiro.filter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.web.filter.PathMatchingFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.chenyan.springshiro.Constants;
+import com.chenyan.springshiro.service.UserService;
+
+/**  
+* <p>Title: SysUserFilter</p>  
+* @author chenyan  
+* @date 2019年8月29日  
+*/
+public class SysUserFilter extends PathMatchingFilter {
+
+	@Autowired
+	private UserService userService ;
+
+	@Override
+	protected boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue)
+			throws Exception {
+		String username = (String)SecurityUtils.getSubject().getPrincipal() ;
+		request.setAttribute(Constants.CURRENT_USER, userService.findByUsername(username)) ;
+		return true ;
+	}
+	
+}
