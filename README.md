d6a84937eef8d22cd08468dcb3e5983e76ebebe4
chenyan <chenyandream@outlook.com>
2019-09-06 09:34:40 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # JavaCourse
-Java学习过程中的一些Demo和心得
+**一些例子：**
+
+**Shiro 部分：**
+
+* **shiro-demo ————   **
+* **shiro-web ———— shiro 与 servlet **
+* **spring-shiro ———— shiro 与 spring**
+
--- spring-shiro/src/main/java/com/chenyan/springshiro/dao/UserDaoImpl.java
+++ spring-shiro/src/main/java/com/chenyan/springshiro/dao/UserDaoImpl.java
@@ -1,10 +1,15 @@
 package com.chenyan.springshiro.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.chenyan.springshiro.entity.User;
@@ -22,32 +27,56 @@ public class UserDaoImpl implements UserDao {
 
 	@Override
 	public User createUser(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		final String sql = "insert into sys_user(organization_id, username, password, salt, role_ids, locked) values(?,?,?,?,?,?)" ;
+		
+		GeneratedKeyHolder keyHolder = new GeneratedKeyHolder() ;
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+				PreparedStatement psst = connection.prepareStatement(sql ,new String[] {"id"}) ;
+				int count = 1 ;
+				psst.setLong(count++ ,user.getOrganizationId()) ;
+				psst.setString(count++ ,user.getUsername()) ;
+				psst.setString(count++ ,user.getPassword()) ;
+				psst.setString(count++ ,user.getSalt()) ;
+				psst.setString(count++ ,user.getRoleIdsStr()) ;
+				psst.setBoolean(count++ ,user.getLocked()) ;
+				return psst ;
+			}
+		} ,keyHolder) ;
+		user.setId(keyHolder.getKey().longValue()) ;
+		return user ;
 	}
 
 	@Override
 	public User updateUser(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "update sys_user set organization_id=?,username=?, password=?, salt=?, role_ids=?, locked=? where id=?" ;
+		jdbcTemplate.update(sql ,
+				user.getOrganizationId() ,user.getUsername() ,user.getPassword() ,user.getSalt() ,user.getRoleIdsStr() ,user.getLocked() ,user.getId()) ;
+		return user ;
 	}
 
 	@Override
 	public void deleteUser(Long userId) {
-		// TODO Auto-generated method stub
-
+		String sql = "delete from sys_user where id=?" ;
+		jdbcTemplate.update(sql ,userId) ;
 	}
 
 	@Override
 	public User findOne(Long userId) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select id, organization_id, username, password, salt, role_ids as roleIdsStr, locked from sys_user where id=?" ;
+        List<User> userList = jdbcTemplate.query(sql, new BeanPropertyRowMapper(User.class), userId) ;
+        if(userList.size() == 0) {
+            return null ;
+        }
+        return userList.get(0) ;
 	}
 
 	@Override
 	public List<User> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select id, organization_id, username, password, salt, role_ids as roleIdsStr, locked from sys_user" ;
+		return jdbcTemplate.query(sql ,new BeanPropertyRowMapper(User.class)) ;
 	}
 
 	@Override
